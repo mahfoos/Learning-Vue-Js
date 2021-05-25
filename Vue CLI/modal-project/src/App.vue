8378992255e96e79d6c746f5fcc53bd416b756ae
Mahfooz <umahfoosahamed@gmail.com>
2021-05-25 06:33:36 +0530

    added components

--- Vue CLI/modal-project/src/App.vue
+++ Vue CLI/modal-project/src/App.vue
@@ -1,15 +1,30 @@
+<!--Root Component-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <Model />
+    <!-- <input type="text" ref = "name">
+    <button @click="handleClick"> Click Me </button>   lesson - 1 -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Model from './components/Model.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Model},
+  data() {
+    return {
+      title: "My First vue App :)"
+    }
+  },
+
+  methods : {
+    // handleClick() {
+    //   console.log(this.$refs.name)
+    //   this.$ref.name.classList.add('active') // adding claas to inout field
+    //   this.$ref.name.focus()
+    //  lesson - 1 }
+
   }
 }
 </script>
@@ -22,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ Vue CLI/modal-project/src/components/Model.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="backrop">
+        <div class="model">
+            <h1>Model Title</h1>
+            <p>Model Content</p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped > /* Only target in this css */
+    .model {
+        width:400px;
+        padding:20px;
+        margin:100px auto;
+        background: grey;
+        border-radius:10px;
+    }
+
+    .backrop {
+        top:0;
+        position:fixed;
+        background:rgba(0,0,0,0,5);
+        width:100%;
+        height:100%;
+    }
+
+    h1 {
+        color: #03cfb4;
+        border: none;
+        padding:0;
+    }
+
+    p{
+        font-style: normal;
+    }
+</style>
